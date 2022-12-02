44c2f967da606e6e20283987b52a5c59238ebc05
ElenaBerestok <98764900+ElenaBerestok@users.noreply.github.com>
2022-12-02 20:50:19 +0200

    Delete PlayButton.js

--- HW3/src/task3/VideoPlayer/PlayButton.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const PlayButton = (props) => {
-    const toggleClass = props.isPlaying ? 'pause' : 'playing';
-    const title = props.isPlaying ? 'Pause' : 'Playing';
-    return <button onClick={props.onClick}className={toggleClass}>{title}</button>
-};
-
-export default PlayButton;
