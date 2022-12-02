3a160519c5fc567445415972be8513dc7e8ba120
ElenaBerestok <98764900+ElenaBerestok@users.noreply.github.com>
2022-12-02 20:01:44 +0200

    Add files via upload

--- HW3/src/task3/VideoPlayer/VideoPlayer.js
+++ HW3/src/task3/VideoPlayer/VideoPlayer.js
@@ -1,61 +1,42 @@
 import React, {useState} from "react";
 import nature from './video/nature.mp4';
 import './VideoPlayerStyle.css';
-import PlayButton from "./PlayButton";
-
-
-// class VideoPlayer extends React.Component{
-//     constructor(props){
-//         super(props);
-//         this.state = {
-//             isPlaying: false,
-//             tracker: 0,
-//         }
-//     }
-
-//     videoRef = React.createRef();
-
-//     handleClick(){
-//         if(this.state.isPlaying) {
-//             this.videoRef.current.pause()
-//         } else {
-//             this.videoRef.current.play()
-//         }
-//         this.setState(prevState => {
-//             return {
-//                 isPlaying: !prevState.isPlaying
-//             };
-//         });
-//     };
-
-//     trackerData(){
-//         const tacker = (this.videoRef.current.currentTime)/(this.videoRef.current.duration)
-//         this.setState({tracker: tacker})
-//     }
-
-
-//     render() {
-
-//         const isPlaying = this.state.isPlaying;
-//         return (
-//             <div className="container">
-//                     <video controls ref={this.videoRef} onTimeUpdate={this.trackerData}>
-//                         <source src={nature} type='video/mp4' />
-//                     </video>
-//                     < PlayButton 
-//                         isPlaying={isPlaying}
-//                         title={isPlaying}
-//                         onClick={this.handleClick.bind(this)}  
-//                     />
-//             </div>
-//         )
-//     }
-// }
 
 const VideoPlayer = () => {
-    const [tracker, setTracker] = useState(0);
+    const [state, setState] = useState({
+        tracker: 0,
+        theRestOfTime: null,
+    });
 
-    const videoRef = React.createRef();
+    const {
+        tracker,
+        theRestOfTime,
+    } = state;
+
+    const videoRef = React.useRef();
+
+    const trackerData = () => {
+        const currentTime = videoRef.current.currentTime;
+        const durationTime = videoRef.current.duration;
+        const theRestOfTime = durationTime - currentTime;
+        theRestOfTime.toFixed(2)
+        setState({
+            tracker: currentTime,
+            theRestOfTime: theRestOfTime.toFixed(2)
+        })
+        
+        return (
+            tracker, 
+            theRestOfTime
+        )
+    }
+
+    const handleChange = (e) => {
+        videoRef.current.seek = e.target.value;
+        videoRef.current.currentTime = videoRef.current.seek;
+       
+        setState({...state})  
+    }
 
     onplay = () => {
         videoRef.current.play()
@@ -65,42 +46,25 @@ const VideoPlayer = () => {
         videoRef.current.pause()
     }
 
-    const trackerData = () => {
-        const currentTime = videoRef.current.currentTime;
-        const derutionTime = videoRef.current.duration;
-        const tracker = (currentTime/derutionTime);
-        setTracker({tracker: tracker})
-        
-        return tracker
-    }
-
-    // const lengthVideo = () => {
-
-    // }
-
-    // const handleTime = () => {
-
-    // }
-
     return (
         <div className="container">
             <div>
-                <video controls ref={videoRef} >
+                <video ref={videoRef} onTimeUpdate={trackerData}>
                     <source src={nature} type='video/mp4' />
                 </video>
             </div>
-            {/* <input 
-                value={tracker}
+            <input 
                 type='range'
                 min = '0'
-                max onDurationChange ={lengthVideo}
-                onChange={handleTime}
-            /> */}
+                max = '15'
+                value={tracker}
+                onChange={(e) => handleChange(e)}
+            />
+            <span>{theRestOfTime}</span>
             <div className="wrapperButton">
                 <button onClick={onplay} className="button">Play</button>
                 <button onClick={onpause} className="button">Pause</button>
             </div>
-           
         </div>
     )
 
