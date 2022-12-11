9c0d32efadad42ec29c7f7768f8ecd8119204db6
ElenaBerestok <98764900+ElenaBerestok@users.noreply.github.com>
2022-12-11 17:34:42 +0200

    Add files via upload

--- HW4/src/index.js
+++ HW4/src/index.js
@@ -3,14 +3,23 @@ import ReactDOM from 'react-dom/client';
 import App from './task1/App';
 // import AppPropsRender from './task3/AppPropsRender'
 import { UsersProvider } from './task1/UsersContext';
+import LoaderHOC from './task2/LoaderHOC';
+import RouterApp from './task4/Router';
+
+import {BrowserRouter as Router} from "react-router-dom"
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <UsersProvider>
+    {/* <UsersProvider>
       <App/>
-    </UsersProvider>
+    </UsersProvider> */}
     {/* <AppPropsRender /> */}
+    <LoaderHOC />
+    {/* <Router>
+      <RouterApp/>
+    </Router> */}
   </React.StrictMode>
   // document.getElementById("root")
 );
